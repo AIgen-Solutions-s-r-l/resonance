5ea4498a559c6a0e76b6ee623f442c3f647136fa
Alessio Rocchi <rocchi.b.a@gmail.com>
2024-11-21 22:01:00 +0100

    Organize imports and improve code readability
    Rearranged and grouped the import statements for better organization. Added blank lines between function definitions and logical code blocks to enhance readability and maintainability.

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
-import logging
 import asyncio
 import json
-from fastapi import FastAPI
+import logging
 from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
+
 from app.core.config import Settings
 from app.core.rabbitmq_client import AsyncRabbitMQClient
 from app.services.lib_document_finder import match_cv
@@ -10,6 +12,7 @@ from app.services.lib_document_finder import match_cv
 logging.basicConfig(level=logging.INFO)
 settings = Settings()
 rabbitmq_client = AsyncRabbitMQClient(rabbitmq_url=settings.rabbitmq_url)
+
 
 async def process_resume_callback(message):
     resume = json.loads(message.body.decode('utf-8'))
@@ -20,8 +23,10 @@ async def process_resume_callback(message):
     await rabbitmq_client.send_message(queue=settings.job_to_apply_queue, message=jobs_to_apply)
     # logging.info(f"Jobs to apply sent to job_to_apply_queue {jobs_to_apply}")
 
+
 async def consume_task():
     await rabbitmq_client.consume_messages(queue=settings.apply_to_job_queue, callback=process_resume_callback)
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -31,7 +36,9 @@ async def lifespan(app: FastAPI):
     task.cancel()
     await rabbitmq_client.close_connection()
 
+
 app = FastAPI(lifespan=lifespan)
+
 
 @app.get("/")
 async def root():
