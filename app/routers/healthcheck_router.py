12f533f660fc65e4288fa0871785aa70fb3f44db
Alessio Rocchi <rocchi.b.a@gmail.com>
2025-02-04 17:12:17 +0100

    ♻️ refactor(healthcheck): remove RabbitMQ health check
    - remove unused RabbitMQ health check for cleaner code
    - delete RabbitMQ-specific health check files to reduce redundancy

--- app/routers/healthcheck_router.py
+++ app/routers/healthcheck_router.py
@@ -2,7 +2,6 @@ from fastapi import APIRouter
 from app.routers.healthchecks.fastapi_healthcheck import HealthCheckFactory, healthCheckRoute
 from app.routers.healthchecks.fastapi_healthcheck_sqlalchemy import HealthCheckSQLAlchemy
 from app.routers.healthchecks.fastapi_healthcheck_mongodb import HealthCheckMongoDB
-from app.routers.healthchecks.fastapi_healthcheck_rabbitmq import HealthCheckRabbitMQ
 from app.core.config import Settings
 from fastapi import HTTPException
 
@@ -32,13 +31,6 @@ async def health_check():
             connection_uri=settings.mongodb,
             alias='mongo db',
             tags=('mongo', 'db')
-        )
-    )
-    _healthChecks.add(
-        HealthCheckRabbitMQ(
-            connection_uri=settings.rabbitmq_url,
-            alias='rabbit mq',
-            tags=('rabbit', 'cache')
         )
     )
     
